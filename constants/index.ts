106bf268424073bbfdf73cb79cd6dbe237daecf9
ENG Abdala Mire <121616854+AbdalaMire@users.noreply.github.com>
2024-09-16 00:50:58 +0300

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,85 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_this_system_work', label: 'How This System Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Real Time Offline Help',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'We provide a solution for you to be able to use our Mobile Application when Walking, offline maps you can use at any time You dont need any signal at the location',
+    },
+    {
+      title: 'Set an Appointment schedule',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Schedule an adventure with friends. On holidays, there are many interesting offers from Our App. After That No More Boring",
+    },
+    {
+      title: 'Technology using Augmented reality',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Technology uses augmented reality as a guide when using our App trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+    },
+    {
+      title: 'Many new locations every Minute',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Lots of new locations every minite, because we have a worldwide community of travelers who share their best experiences with traveling',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About This',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Travel To World', 'Know new things', 'Learn Culture'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Abdala Mire', value: '+251-917752620' },
+      { label: 'Email', value: 'abdalamire2843@gmail.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+    ],
+  };
